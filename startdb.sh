f80a32b776e978b69d17bd85dc6fff07a1beb10d
lychheang <lychheang008@gmail.com>
2023-07-24 16:21:32 +0700

    final test1

--- startdb.sh
+++ startdb.sh
@@ -3,6 +3,6 @@
 docker build -t mydatabase mydata/.
 docker build -t phpmyadmin1 phpApp/.
 
-docker run -d --name=myDB -v ./mydata:/config -p 5001:3306 --restart unless-stopped mydatabase
+docker run -d --name=myDB -v ./mydata:/config -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro -p 5001:3306 --restart unless-stopped mydatabase
 
 docker run --name phpmyadmin -d --link myDB:db -p 80:80 phpmyadmin1
