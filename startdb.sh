702803f3e1bbf2fc7607338d75b3b0d687d5ded8
lychheang <lychheang008@gmail.com>
2023-07-24 17:32:12 +0700

    fix phpstart

--- startdb.sh
+++ startdb.sh
@@ -5,4 +5,4 @@ docker build -t phpmyadmin1 phpApp/.
 
 docker run -d --name=myDB -v ./mydata:/config -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro -p 5001:3306 --restart unless-stopped mydatabase
 
-docker run --name phpmyadmin -d --link myDB:db -p 80:80 phpmyadmin1
+docker run --name phpmyadmin -d --link myDB:db -p 80:80 --restart unless-stopped phpmyadmin1
