c046fd21a33536ee5306f55bcf7b52baa2109ce1
lychheang <lychheang008@gmail.com>
2023-07-26 13:47:08 +0700

    try flaskapp

--- startdb.sh
+++ startdb.sh
@@ -6,3 +6,5 @@ docker build -t phpmyadmin1 phpApp/.
 docker run -d --name=myDB -v ./mydata:/config -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro -p 5001:3306 --restart unless-stopped mydatabase
 
 docker run --name phpmyadmin -d --link myDB:db -p 80:80 --restart unless-stopped phpmyadmin1
+
+docker run -d --name=myweb -p 81:81 --restart unless-stopped lychheang008/my_web_flask
